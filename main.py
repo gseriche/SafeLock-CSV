80c90783253c650f79f615c9e6056fdf08d06f24
Gonzalo Seriche <gonzalo.seriche@thebridge.social>
2022-06-20 21:29:01 -0400

    Read and process from SafeLock Enforcer csv

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import csv
+import pandas as pd
+
+csv_done = "./approve-list-done"
+csv_path = "./approve-list-csv"
+csv_files = os.listdir(csv_path)
+
+
+header = ['Full File Path']
+for file in csv_files:
+    with open(csv_path+"/"+file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        TempOnlyFilePath = list()
+        for row in csv_reader:
+            filePath = row[0].split('\\')
+            filePath.pop()
+            filepath = '\\'.join(filePath)
+            TempOnlyFilePath.append(filepath)
+        onlyFilePath = list(dict.fromkeys(TempOnlyFilePath))
+    
+
+    data = pd.DataFrame(onlyFilePath, columns=header)
+    data.to_csv(csv_done+"/"+'done-'+file, index=False)
+
+
